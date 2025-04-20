dd5350949a4f5f9521605a7f21b706b7f2f3715f
Waseem <wyameen152@gmail.com>
2025-04-20 12:27:40 +0500

    Added MongoDB connection in Project

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const urlRouter = require('./router/UrlRouter')
+const db = require('./services/db')
 
 const app = express()
+app.use(bodyParser)
 
+//router
 app.use(urlRouter)
 
 app.listen(5000, ()=>{
